da777e45eb948597b1fd661dd4cac31660e172f7
Oleg Vlasenko <vop@unity.net>
2021-07-20 22:09:57 +0300

    Added provider token

--- invars.tf
+++ invars.tf
@@ -1,3 +1,8 @@
+
+variable "token" {
+  description = "Hetzner Cloud API token"
+}
+
 variable "ssh_key_name" {
    description = "Name of public key at Hetzner cloud for WebAdmin"
    default = "hcloud_admin"
